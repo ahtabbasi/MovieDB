81602963219103fb5eb6280c0af349fe1d737b95
Ahtsham Abbasi <a_abbasi@flo.health>
2025-06-29 22:15:31 +0200

    Upgrade to AGP to 7.4.2

--- app/src/main/java/com/kshitijchauhan/haroldadmin/moviedb/ui/main/MainActivity.kt
+++ app/src/main/java/com/kshitijchauhan/haroldadmin/moviedb/ui/main/MainActivity.kt
@@ -120,7 +120,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_activity_main, menu)
         return true
     }
